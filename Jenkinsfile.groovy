7cc3e662db7fc7ef1aa22d4cb1683309987302e4
Helbert Rico <venmkhri@l0t440pf02bbe2.na.rccl.com>
2019-06-06 17:53:31 -0400

    Adding maven command

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,7 +4,11 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                echo 'Building..'
+       		dir ( 'sfdc-www' ) {
+                	withMaven(maven: 'mvn311'){
+                    		sh 'mvn clean package site -Dmaven.site.skip=false -DstagingDirectory=${WORKSPACE}/report -Dnexus.site.url=http://nexusprod:8080/nexus/content/groups/public -Ddependency.locations.enabled=false -T 1C --no-snapshot-updates'
+                	}
+		}
             }
         }
         stage('Test') {
